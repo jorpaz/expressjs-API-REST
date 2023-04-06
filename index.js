99742e800930cb2eb8cef664e7322ec5ff28819c
jorpaz <giovanny.paz@outlook.com>
2023-04-06 12:30:24 -0600

    Routing express.Router

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
@@ -10,25 +12,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json([{
-    name: 'producto 1',
-    price: 1000},
-    {
-      name: 'producto 2',
-      price: 2000
-    }
-  ]);
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'producto 2',
-    price: 2000
-  });
-});
+routerApi(app);
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId , productId } = req.params;
