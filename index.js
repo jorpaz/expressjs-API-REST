727a60ecb9096d7b417cffdb902f4eea721ee99a
jorpaz <giovanny.paz@outlook.com>
2023-04-07 16:19:47 -0600

    middlewares config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,10 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
--- services/productService.js
+++ services/productService.js
@@ -1,4 +1,5 @@
 const {faker} = require("@faker-js/faker");
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -16,6 +17,7 @@ class ProductsService {
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean(),
       });
     }
   }
@@ -38,14 +40,22 @@ class ProductsService {
   }
 
   async findOne(id) {
-  return this.products.find(item => item.id === id)
+  const product = this.products.find(item => item.id === id);
+
+  if (!product){
+    throw boom.notFound('Product Not Found');
+  }
+  if (product.isBlock){
+    throw boom.conflict('Product is block');
+  }
+  return product;
   }
 
   async update(id, changes) {
   const index = this.products.findIndex(item => item.id === id);
 
   if (index === -1){
-    throw new Error('Product Not Found');
+    throw boom.notFound('Product Not Found');
   }
   const product = this.products[index];
   this.products[index] = {
@@ -59,7 +69,7 @@ class ProductsService {
     const index = this.products.findIndex(item => item.id === id);
 
     if (index === -1){
-      throw new Error('Product Not Found');
+      throw boom.notFound('Product Not Found');
     }
     this.products.splice(index, 1);
     return { id };
